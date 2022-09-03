5db22e683b69a47ebcb71ec2b3d41e9543cf5e7a
Shashank Shirke <shashankshirke285@gmail.com>
2022-09-03 03:35:58 -0700

    fix: moved out the downloader function to ajax.php

--- ais.php
+++ ais.php
@@ -54,22 +54,6 @@ class AIS{
         return true;
     }
     
-    $asin = $_POST['asin'];
-if (!ctype_alnum($asin))
-{
-    die('invalid_input');
-}
-
-
-$AIS = new AIS('downloads');
-
-$success = $AIS->downloadImages($asin);
-
-if($success){
-    echo 'success';
-}else{
-    echo 'failed';
-
     private function getHighestResolutionImageUrl($data){
         if($data->{'hiRes'}){
             return $data->{'hiRes'};
